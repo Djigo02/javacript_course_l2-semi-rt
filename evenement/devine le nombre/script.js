55ab45634e489ba7ed20a97ca39a9937b16afebe
Mouhamad DJIGO <mehmetdjigo@gmail.com>
2025-12-01 09:59:42 +0000

    Guess number

--- evenement/devine le nombre/script.js
+++ evenement/devine le nombre/script.js
@@ -1,1 +1,43 @@
-console.log('Devine le nombre!');
+const guess = document.querySelector(".guess");
+const chNumberSecret = document.querySelector(".number");
+const btnAgain = document.querySelector(".again");
+const btnCheck = document.querySelector(".check");
+const message = document.querySelector(".message");
+
+var secret = Math.floor(Math.random() * 100) + 1;
+
+btnCheck.addEventListener("click", () => {
+  if (isNaN(Number(guess.value))) {
+    messageValue("Le nombre a deviner est entre 1 et 100");
+  } else {
+    if (guess.value < secret) {
+      messageValue("Le nombre a deviner est plus grand");
+    } else if (guess.value > secret) {
+      messageValue("Le nombre a deviner est plus petit");
+    } else {
+      messageValue("Bravo 🎉");
+      chNumberSecret.textContent = guess.value;
+      //   document.body.style.backgroundColor = "#52a938ff";
+    }
+  }
+});
+
+btnAgain.addEventListener("click", () => {
+  reset();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  reset();
+});
+
+function reset() {
+  guess.value = "";
+  chNumberSecret.textContent = "?";
+  message.textContent = "Demmarage ...";
+  //   document.body.style.backgroundColor = "";
+  document.body.classList.add("my-element");
+}
+
+function messageValue(value) {
+  message.textContent = value;
+}
